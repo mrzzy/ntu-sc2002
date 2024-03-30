ae1bc5643797d6a493629bf658b6272189c6c356
Zhu Zhanyan <program.nom@gmail.com>
2024-03-30 16:57:58 +0800

    chore: rename App class to Application to match class diagram

--- src/main/java/sg/edu/ntu/sc2002/Application.java
+++ src/main/java/sg/edu/ntu/sc2002/Application.java
@@ -1,7 +1,7 @@
 package sg.edu.ntu.sc2002;
 
 /** Hello world! */
-public class App {
+public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
