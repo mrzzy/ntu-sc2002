fd02367975aa6030e8941ea348d0ce0499c37fa6
lczm <zemingchua@gmail.com>
2024-04-15 14:08:20 +0800

    Implement payment class

--- src/main/java/sg/edu/ntu/sc2002/Payment.java
+++ src/main/java/sg/edu/ntu/sc2002/Payment.java
@@ -1,2 +1,21 @@
-package sg.edu.ntu.sc2002;public class Payment {
+package sg.edu.ntu.sc2002;
+
+public class Payment implements PaymentMethod{
+    /**
+     * Charge the payment method to pay for given amount.
+     *
+     * @param amountCents Amount to charge in cents.
+     * @return {@code true} If the charge was successful, false otherwise.
+     */
+
+    private String name;
+
+    public Payment(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean pay(int amountCents) {
+        return false;
+    }
 }
