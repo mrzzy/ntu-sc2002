794e534280b9f3b57271ea587dd7585fa97d4893
lczm <zemingchua@gmail.com>
2024-04-15 14:03:42 +0800

    (Admin) Implement transferStaff

--- src/main/java/sg/edu/ntu/sc2002/AdminAction.java
+++ src/main/java/sg/edu/ntu/sc2002/AdminAction.java
@@ -239,6 +239,110 @@ public class AdminAction implements Action {
         System.out.printf("%s has been promoted from staff to manager at branch %s\n", selectedUser.getUsername(), selectedBranch.getName());
     }
 
+    private void transferStaff(Scanner in, Chain chain) {
+        System.out.println("Transfer staff");
+
+        // Get branch FROM
+        System.out.println("Please select from which branch you want to transfer from: ");
+        System.out.println("Available branches : ");
+        for (Branch b: chain.getBranches()) {
+            System.out.println(b.getName());
+        }
+
+        String branchFrom = in.next();
+        Branch selectedFromBranch = null;
+        for (Branch b : chain.getBranches()) {
+            if (b.getName().equals(branchFrom)) {
+                selectedFromBranch = b;
+                break;
+            }
+        }
+
+        if (selectedFromBranch == null) {
+            System.out.println("The branch selected does not exist");
+            return;
+        }
+
+        // Get a staff/manager from branch FROM
+        System.out.println("Please select the staff/manager username you want to transfer : ");
+        System.out.println("Available staff : ");
+        for (User user : selectedFromBranch.getStaffs()) {
+            System.out.println(user.getUsername());
+        }
+        for (User user : selectedFromBranch.getManagers()) {
+            System.out.println(user.getUsername());
+        }
+
+        String username = in.next();
+        User selectedUser = null;
+        for (User user : selectedFromBranch.getStaffs()) {
+            if (user.getUsername().equals(username)) {
+                selectedUser = user;
+                break;
+            }
+        }
+        for (User user : selectedFromBranch.getManagers()) {
+            if (user.getUsername().equals(username)) {
+                selectedUser = user;
+                break;
+            }
+        }
+
+        if (selectedUser == null) {
+            System.out.println("Selected user does not exist");
+            return;
+        }
+
+        // Get branch To
+        System.out.println("Please select from which branch you want to transfer to:");
+        System.out.println("Available branches : ");
+        for (Branch b: chain.getBranches()) {
+            System.out.println(b.getName());
+        }
+
+        String branchTo = in.next();
+        Branch selectedToBranch = null;
+        for (Branch b : chain.getBranches()) {
+            if (b.getName().equals(branchTo)) {
+                selectedToBranch = b;
+                break;
+            }
+        }
+
+        if (selectedToBranch == null) {
+            System.out.println("The branch selected does not exist");
+            return;
+        }
+
+        // Check that the from and to branch is not the same
+        if (branchFrom == branchTo) {
+            System.out.println("The branch you are transferring from and to is the same!");
+            return;
+        }
+
+        // If it's a staff, check the quota limit
+        if (selectedUser.getRole().code() == 'S') {
+            if (selectedToBranch.getStaffs().size() >= selectedToBranch.getStaffQuota()) {
+                System.out.println("The branch you selected to transfer to has already has maxed out their staff quota");
+                return;
+            }
+            // Transfer
+            selectedToBranch.getStaffs().add(selectedUser);
+            selectedFromBranch.getStaffs().remove(selectedUser);
+        }
+        // If it's a manager, check the manager quota limit
+        if (selectedUser.getRole().code() == 'M') {
+            if (selectedToBranch.getManagers().size() >= selectedToBranch.getManagerQuota()) {
+                System.out.println("The branch you selected to transfer to has already haxed out their manager quota");
+                return;
+            }
+            selectedToBranch.getManagers().add(selectedUser);
+            selectedFromBranch.getStaffs().remove(selectedUser);
+        }
+
+        System.out.printf("Successfully transferred %s from %s to %s\n", selectedUser.getUsername(), selectedFromBranch.getName(), selectedToBranch.getName());
+    }
+
     // TODO : Exception here is not intended long term
     private void openBranch(Scanner in, Chain chain) {
         System.out.println("Open branch");
@@ -304,6 +408,7 @@ public class AdminAction implements Action {
             case LIST_STAFF_ALL -> listAllStaff(in, chain);
             case ASSIGN_MANAGER -> assignManager(in, chain);
             case PROMOTE_STAFF -> promoteStaff(in, chain);
+            case TRANSFER_STAFF -> transferStaff(in, chain);
             case OPEN_BRANCH -> openBranch(in, chain);
             case CLOSE_BRANCH -> closeBranch(in, chain);
         }
