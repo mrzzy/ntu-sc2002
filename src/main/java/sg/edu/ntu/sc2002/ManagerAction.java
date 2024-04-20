22fac15e3f57e495188f656e9958b11c975e635c
QGBSA <qgbsa555@gmail.com>
2024-04-20 14:57:15 +0800

    add ManagerAction.java

---
+++ src/main/java/sg/edu/ntu/sc2002/ManagerAction.java
@@ -0,0 +1,27 @@
+/*
+ * NTU SC2002 project
+ * ManagerAction
+ */
+
+package sg.edu.ntu.sc2002;
+
+import java.util.Scanner;
+
+/** Defines an executable Action on a Fast Food {@link Chain}. */
+public interface ManagerAction {
+    /**
+     * Title of the Manager Action displayed in the user interface.
+     *
+     * @return Tile of the manager action.
+     */
+    public String title();
+
+    /**
+     * Execute Action on the given Fast Food Chain.
+     *
+     * @param in     Stdin scanner used by action to read user input.
+     * @param branch The particular branch the manager perform the action on.
+     * @return State of the branch post performing action.
+     */
+    public Branch execute(Scanner in, Branch branch);
+}
