5b900a75f944b7affb6eb293759adb9a925a0f38
Zhu Zhanyan <program.nom@gmail.com>
2024-04-21 15:43:22 +0800

    test: serialisation of Orders in Chain

--- src/test/java/sg/edu/ntu/sc2002/SerializeTest.java
+++ src/test/java/sg/edu/ntu/sc2002/SerializeTest.java
@@ -13,13 +13,32 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SerializeTest {
+    private Chain chain = Init.initChain();
+
+    @BeforeEach
+    public void setup() {
+        Branch branch = chain.getBranches().iterator().next();
+        branch.getOrders()
+                .putAll(
+                        Stream.of(
+                                        new Order(
+                                                new ArrayList<>(branch.getMenu()),
+                                                DiningOption.DINE_OUT))
+                                .collect(Collectors.toMap(o -> o.getId(), Function.identity())));
+    }
+
     @Test
     public void save() throws IOException {
         File saveFile = File.createTempFile("chain", "");
-        Serialize.save(Init.initChain(), saveFile.getPath());
+        Serialize.save(chain, saveFile.getPath());
         assertTrue(Files.size(Path.of(saveFile.getPath())) > 0);
         saveFile.delete();
     }
@@ -27,11 +46,10 @@ class SerializeTest {
     @Test
     public void restore() throws IOException {
         File saveFile = File.createTempFile("chain", "");
-        Chain saveChain = Init.initChain();
-        Serialize.save(saveChain, saveFile.getPath());
+        Serialize.save(chain, saveFile.getPath());
 
         Chain restoreChain = Serialize.restore(saveFile.getPath());
-        assertEquals(saveChain, restoreChain);
+        assertEquals(chain, restoreChain);
         saveFile.delete();
     }
 }
