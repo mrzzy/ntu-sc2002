40d47334d14d66a6ea9529b5ad8f8041e70f1b0f
ernyo <eyong002@gmail.com>
2024-04-22 15:29:56 +0800

    add customisation edit

--- src/main/java/sg/edu/ntu/sc2002/CustomerOrderMethod.java
+++ src/main/java/sg/edu/ntu/sc2002/CustomerOrderMethod.java
@@ -3,6 +3,7 @@ package sg.edu.ntu.sc2002;
 public enum CustomerOrderMethod {
     ADD_TO_CART, // Add item to cart
     REMOVE_FROM_CART, // Remove item from cart
+    CUSTOMISE_ITEM_IN_CART, // Customise item in cart
     VIEW_CART, // View items in cart
     PAY // pay for the order
 }
